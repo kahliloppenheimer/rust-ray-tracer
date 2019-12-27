66aabdbcaf8211082a1502a317635a6f405104e7
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2019-12-27 15:45:03 -0500

    fiddling around

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    const N: i32 = 5;
+    let x: i32 = 100;
+    let add = |a, b| a + b;
+    println!("{} + {} = {} {}", N, x, sum(N, x), add(N, x));
+}
+
+fn sum(a: i32, b: i32) -> i32 {
+  a + b
 }
